249cd23cf3ff789e52874b65d4279e034d98e706
Woody <143530939+JackDee84@users.noreply.github.com>
2025-06-03 17:27:55 +0200

    commit 1

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+
+import { defineCollection, z } from 'astro:content';
+
+import { glob } from 'astro/loaders';
+
+const cardPost = defineCollection ({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/cards" }),
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    description: z.string(),
+    cover: image().optional(),
+    coverAlt: z.string().optional()
+  })
+})
+
+export const collections = { cardPost }
+
+
