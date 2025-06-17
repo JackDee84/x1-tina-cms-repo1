ac026bf783cee941c574c056bf5d9985e9428155
Woody <143530939+JackDee84@users.noreply.github.com>
2025-06-17 21:03:48 +0200

    sss

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ import { glob } from 'astro/loaders';
 const cardPost = defineCollection ({
   loader: glob({ pattern: "**/*.mdoc", base: "src/content/cards" }),
   schema: ({ image }) => z.object({
-    title: z.string() ,
+    title: z.string() ,  
     // description: z.string(),
     //heroImage: image().optional(),
     // coverAlt: z.string().optional()
