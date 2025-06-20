167073d2088dbda31f6a094a6349c2a41da884c1
Woody <143530939+JackDee84@users.noreply.github.com>
2025-06-20 10:17:32 +0200

    fdfdfd

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const cardPost = defineCollection ({
-  loader: glob({ pattern: "**/*.mdoc", base: "src/content/cards" }),
+  //loader: glob({ pattern: "**/*.mdoc", base: "src/content/cards" }),
   schema: ({ image }) => z.object({
     title: z.string() ,  
     // description: z.string(),
@@ -12,6 +12,7 @@ const cardPost = defineCollection ({
     // coverAlt: z.string().optional()
     heroImage: image().optional(), // `image()` ist ein spezieller Zod-Typ von Astro, der das Bild verarbeitet
     heroImageAlt: z.string().optional(), // Für den Alt-Text
+    content: z.string(),
   })
 })
 
